42ebd34e120cff6e2d3fa3b546427e0ec35fdf7c
Ioan Loosley <legit.ioangogo@gmail.com>
2020-02-20 21:01:25 +0000

    sleep checking

--- SDS011.cpp
+++ SDS011.cpp
@@ -32,6 +32,28 @@ static const byte SLEEPCMD[19] = {
 	0xAB	// tail
 };
 
+static const byte WAKECMD[19] = {
+	0xAA,	// head
+	0xB4,	// command id
+	0x06,	// data byte 1
+	0x01,	// data byte 2 (set mode)
+	0x01,	// data byte 3 (wake up)
+	0x00,	// data byte 4
+	0x00,	// data byte 5
+	0x00,	// data byte 6
+	0x00,	// data byte 7
+	0x00,	// data byte 8
+	0x00,	// data byte 9
+	0x00,	// data byte 10
+	0x00,	// data byte 11
+	0x00,	// data byte 12
+	0x00,	// data byte 13
+	0xFF,	// data byte 14 (device id byte 1)
+	0xFF,	// data byte 15 (device id byte 2)
+	0x06,	// checksum
+	0xAB	// tail
+};
+
 SDS011::SDS011(void) {
 
 }
@@ -79,21 +101,42 @@ int SDS011::read(float *p25, float *p10) {
 // SDS011:sleep
 // --------------------------------------------------------
 void SDS011::sleep() {
+	bool sleepNotConfirmed = true;
+	byte buffer[10];
+	while (sleepNotConfirmed){
+		for (uint8_t i = 0; i < 19; i++) {
+			sds_data->write(SLEEPCMD[i]);
+		}
+		sds_data->flush();
+
+		int len = 0;
+		while ((sds_data->available() > 0) && (sds_data->available() >= (10-len))) {
+			buffer[len]sds_data->read();
+			len++;
+			if(len == 10){
+				bool commandCheck = buffer[1] == 0xC5;
+				bool stateCheck = buffer[3] == 0x01;
+				if(commandCheck && stateCheck){
+					sleepNotConfirmed = false;
+				}
+			}
+		}
+	}
+}
+
+
+// --------------------------------------------------------
+// SDS011:wakeup
+// Fix from: https://github.com/ricki-z/SDS011/issues/21
+// --------------------------------------------------------
+void SDS011::wakeup() {
 	for (uint8_t i = 0; i < 19; i++) {
-		sds_data->write(SLEEPCMD[i]);
+		sds_data->write(WAKECMD[i]);
 	}
 	sds_data->flush();
 	while (sds_data->available() > 0) {
 		sds_data->read();
 	}
-}
-
-// --------------------------------------------------------
-// SDS011:wakeup
-// --------------------------------------------------------
-void SDS011::wakeup() {
-	sds_data->write(0x01);
-	sds_data->flush();
 }
 
 #ifndef ESP32
